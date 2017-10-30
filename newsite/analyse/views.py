3c7acd6e89daa499d508a1386fd7d63a6401a9d8
MJ-Singh <singhmanjeet1998ms@gmail.com>
2017-10-30 21:17:52 +0530

    code structure perfectly ready

--- newsite/analyse/views.py
+++ newsite/analyse/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponse
 # data science libraries
 from .functions import patient_traffic
 import pandas as pd
+import os
+
 data = pd.read_csv('/home/itachi/newsite/static/data.csv')
 
 
@@ -18,10 +20,19 @@ def index(request):
 		subcate1 = request.POST.get("subcategory1", "")
 		subcate2 = request.POST.get("subcategory2", "")
 		subcate3 = request.POST.get("subcategory3", "")
-		var  = compute(maincate, subcate1)
-		fig  = patient_traffic(data, subcate1)
-		plots = [maincate, subcate1, subcate2, subcate3]
-		return render(request, 'analyse/index.html', {"plots": plots, "fig": fig})
+		plot = patient_traffic(data, subcate1)
+		
+		BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+		STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+		img_path = os.path.join(STATIC_ROOT, 'analyse/images')
 
-def compute(a,b):
-	return a + b
+		plot.savefig(os.path.join( img_path, str(subcate1)+str(".png")))
+
+		path = os.path.join( 'analyse/images', str(subcate1)+str(".png")) #'images/filename.png'
+
+		plots = [maincate, subcate1, subcate2, subcate3, path]
+
+		return render(request, 'analyse/index.html', {"plots": plots, "path": path})
+
+def compute():
+	return 'a + b'
